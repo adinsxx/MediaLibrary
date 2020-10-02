0f6240d48b38d967d1a9fab8516128e0ecca4859
Connor <c.mccurrie@gmail.com>
2020-10-01 20:12:19 -0500

    Added previous MovieFile class to edit

--- MovieFile.cs
+++ MovieFile.cs
@@ -2,6 +2,7 @@ using System;
 using NLog.Web;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MediaLibrary
 {
@@ -13,7 +14,95 @@ namespace MediaLibrary
         public List<Movie> Movies { get; set; }
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
 
+        //Constructor
+        public MovieFile(string movieFileListing)
+        {
+            listing = movieFileListing;
+            Movies = new List<Movie>();
+
+            //Read from file
+            // to populate the list with data, read from the data file
+            try
+            {
+                StreamReader sr = new StreamReader(listing);
+                // first line contains column headers
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    // create instance of Movie class
+                    Movie movie = new Movie();
+                    string line = sr.ReadLine();
+                    // first look for quote(") in string
+                    // this indicates a comma(,) in movie title
+                    int idx = line.IndexOf('"');
+                    if (idx == -1)
+                    {
+                        // no quote = no comma in movie title
+                        // movie details are separated with comma(,)
+                        string[] movieDetails = line.Split(',');
+                        movie.mediaId = UInt64.Parse(movieDetails[0]);
+                        movie.title = movieDetails[1];
+                        movie.genres = movieDetails[2].Split('|').ToList();
+                    }
+                    else
+                    {
+                        // quote = comma in movie title
+                        // extract the movieId
+                        movie.mediaId = UInt64.Parse(line.Substring(0, idx - 1));
+                        // remove movieId and first quote from string
+                        line = line.Substring(idx + 1);
+                        // find the next quote
+                        idx = line.IndexOf('"');
+                        // extract the movieTitle
+                        movie.title = line.Substring(0, idx);
+                        // remove title and last comma from the string
+                        line = line.Substring(idx + 2);
+                        // replace the "|" with ", "
+                        movie.genres = line.Split('|').ToList();
+                    }
+                    Movies.Add(movie);
+                }
+                // close file when done
+                sr.Close();
+                logger.Info("Movies in file {Count}", Movies.Count);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+        // public method
+        public bool isUniqueTitle(string title)
+        {
+            if (Movies.ConvertAll(m => m.title.ToLower()).Contains(title.ToLower()))
+            {
+                logger.Info("Duplicate movie title {Title}", title);
+                return false;
+            }
+            return true;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            try
+            {
+                // first generate movie id
+                movie.mediaId = Movies.Max(m => m.mediaId) + 1;
+                StreamWriter sw = new StreamWriter(listing, true);
+                sw.WriteLine($"{movie.mediaId},{movie.title},{string.Join("|", movie.genres)}");
+                sw.Close();
+                // add movie details to Lists
+                Movies.Add(movie);
+                // log transaction
+                logger.Info("Movie id {Id} added", movie.mediaId);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
 
 
     }
+
 }
